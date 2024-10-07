c6457888cfe20f43c15055063b16b601e91c3cb0
KrasenCheneshkov <krasen.cheneshkov@gmail.com>
2024-10-07 16:27:39 +0300

    Updated the game logic.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,21 +1,27 @@
 import random
+def game_head():
+    print(f'{'=' * 25}')
+    print('Welcome player!')
+    print('In this game the computer has chosen a number and you have to guess it!')
+    print(f'{'=' * 25}')
 
-def guess_a_number(level: int) -> str:
+def number_guess(level: int):
     top_limit = 100 * level
     number_to_guess = random.randint(1, top_limit)
+
     while True:
         player_guess = int(input(f'Please enter your guess (1-{top_limit}): '))
         if number_to_guess == player_guess:
             break
         elif player_guess > number_to_guess:
-            print('The correct number is lower than your guess.')
+            print('The correct number is LOWER than your guess.')
         elif player_guess < number_to_guess:
-            print('The correct number is higher than your guess.')
-    return 'Congratulations! Your guess was correct!'
+            print('The correct number is HIGHER than your guess.')
 
-def stay_in_game_question() -> bool:
-    stay_in_game = input('Would you like to play the next level? (Y/N): ')
-    if stay_in_game == 'Y':
+    print('Congratulations! Your guess was correct!')
+
+def stay_in_game_question(answer) -> bool:
+    if answer == 'Y':
         return True
     return False
 
@@ -23,18 +29,17 @@ def game_body():
     level = 1
     while True:
         print(f'=== LEVEL {level} ===')
-        guess_a_number(level)
+        number_guess(level)
         print(f'{'=' * 15}\n')
-        stay_in_game_question()
-        if stay_in_game_question():
+
+        stay_in_game = input('Would you like to play the next level? (Y/N): ')
+        if stay_in_game_question(stay_in_game):
             level += 1
+            continue
         else:
             print('*** Thank you for playing! ***')
             print('*** BYE! ***')
             break
-print(f'{'=' * 25}')
-print('Welcome player!')
-print('In this game the computer has chosen a number and you have to guess it!')
-print(f'{'=' * 25}')
-
+game_head()
 game_body()
+
